a972ffe0d24dc8111b353b3478d13697d7358ba2
Isabela Lugo <marinpereirajob@gmail.com>
2025-03-25 10:53:40 -0300

    learn(ap_08): encerra a apostila

--- ap_08_estruturas_sequenciais/script.sql
+++ ap_08_estruturas_sequenciais/script.sql
@@ -1,18 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 --1.7 Escreva um programa que gere um inteiro que representa o ano de nascimento de uma pessoa no intervalo [1980, 2000] e gere um inteiro que representa o ano atual no intervalo[2010, 2020]. O programa deve exibir a idade da pessoa em anos. Desconsidere detalhes envolvendo dias, meses, anos bissextos etc.
 DO $$
 DECLARE
