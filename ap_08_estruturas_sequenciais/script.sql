746e2035f5019db3983cace3553c159868b1f156
Isabela Lugo <marinpereirajob@gmail.com>
2025-03-25 10:48:49 -0300

    learn(ap_09): termina a apostila

--- ap_08_estruturas_sequenciais/script.sql
+++ ap_08_estruturas_sequenciais/script.sql
@@ -1,29 +1,178 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--1.7 Escreva um programa que gere um inteiro que representa o ano de nascimento de uma pessoa no intervalo [1980, 2000] e gere um inteiro que representa o ano atual no intervalo[2010, 2020]. O programa deve exibir a idade da pessoa em anos. Desconsidere detalhes envolvendo dias, meses, anos bissextos etc.
 DO $$
 DECLARE
-    n1 NUMERIC (5, 2);
-    n2 INTEGER;
-    limite_inferior INTEGER := 5;
-    limite_superior INTEGER := 17;
+  nasci INTEGER;
+  atual INTEGER;
+  idade INTEGER;
+  limite_inferior_N INTEGER := 1980;
+  limite_superior_N INTEGER := 2000;
+  limite_inferior_A INTEGER := 2010;
+  limite_superior_A INTEGER := 2020;
 BEGIN
--- 0 <= n1 < 1 (real) entre 0 e 1 execto o 1 o zero incluso o 1 nao 
-    n1 := random();
-    RAISE NOTICE 'n1 := radom apenas entre 0 e 1 execto o 1 o zero incluso o 1 nao : %', n1;
--- 1 <= n1 < 10 (real) nao inclui o 10 inclui no minimo o 1
-    n1 := 1 + random() * 9 ;
-    RAISE NOTICE 'n1: 1+radom*9 nao inclui o 10 tem como minimo 1 :  %', n1;
--- 1 <= n2 <10 (:: faz type cast é uma função que converte um valor de um tipo de dado para outro ) (floor arredonda para baixo) cortar a parte decimal
-    n2 := floor(random() * 10 + 1)::int;
-    RAISE NOTICE 'n2 cortar a parte decimal : %', n2;
--- limite_inferior <= n2 <= limite_superior  superior menos o inferior mais um vai dar ate 12.999 mas com tem o chao fica 12 nao chega ate o 13 no caso 13 numros acima do 5 que é o 16.9999 ou 16 com o chao 
-    n2 := floor(random() * (17 - 5 + 1) + 5)::int;
-    RAISE NOTICE '%', n2;
--- limite_inferior <= n2 <= limite_superior
-    n2 := floor(random() * (limite_superior - limite_inferior + 1) + limite_inferior)::int;
-    RAISE NOTICE '%', n2;
+  nasci := floor(random() * (limite_superior_N - limite_inferior_N + 1) + limite_inferior_N)::INTEGER;
 
+  atual := floor(random() * (limite_superior_A - limite_inferior_A + 1) + limite_inferior_A)::INTEGER;
+
+  idade := atual - nasci;
+
+  RAISE NOTICE 'Ano de nascimento: %, Ano atual: %, Idade: %', nasci, atual, idade;
 END $$;
 
 
+--1.6 Faça um programa que gere medidas reais de um terreno retangular. Gere também um valor real no intervalo [60, 70] que representa o preço por metro quadrado. O programa deve exibir o valor total do terreno.
+DO $$
+DECLARE
+  nAltura NUMERIC(5, 2);
+  nBase NUMERIC(5, 2);
+  preco_m2 NUMERIC(5, 2);
+  area NUMERIC(7, 2);
+  valor_terreno NUMERIC(10, 2);
+  limite_inferior NUMERIC := 1;
+  limite_superior NUMERIC := 10;
+  limite_inferior_m2 NUMERIC := 60;
+  limite_superior_m2 NUMERIC := 70;
+BEGIN
+  nAltura := (random() * (limite_superior - limite_inferior)) + limite_inferior;
+  nBase := (random() * (limite_superior - limite_inferior)) + limite_inferior;
+
+  preco_m2 := (random() * (limite_superior_m2 - limite_inferior_m2)) + limite_inferior_m2;
+
+  area := nAltura * nBase;
+
+  valor_terreno := area * preco_m2;
+
+  RAISE NOTICE 'Altura: %, Base: %, Preço por m²: %, Área: %, Valor total: %', 
+    nAltura, nBase, preco_m2, area, valor_terreno;
+END $$;
+
+
+--1.5 Faça um programa que gere um número inteiro e mostre a raiz cúbica de seu antecessor e a raiz quadrada de seu sucessor.
+DO $$
+DECLARE
+  n2 INTEGER;
+  limite_inferior INTEGER := 1;
+  limite_superior INTEGER := 100;
+  antecessor INTEGER;
+  sucessor INTEGER;
+  raiz_quadrada DOUBLE PRECISION;
+  raiz_cubica DOUBLE PRECISION;
+BEGIN
+  n2 := floor(random() * (limite_superior - limite_inferior + 1) + limite_inferior)::INTEGER;
+
+  antecessor := n2 - 1;
+  sucessor := n2 + 1;
+
+  raiz_cubica := power(antecessor, 1.0 / 3.0);
+  raiz_quadrada := sqrt(sucessor);
+
+  RAISE NOTICE 'n2: %, antecessor: %, sucessor: %, raiz quadrada: %, raiz cúbica: %', 
+  n2, antecessor, sucessor, raiz_quadrada, raiz_cubica;
+END $$;
+
+
+--1.4 Faça um programa que gere três valores reais a, b, e c e mostre o valor de delta: aquele que calculamos para chegar às potenciais raízes de uma equação do segundo grau.
+DO $$
+DECLARE
+  nA NUMERIC(5, 2);
+  nB NUMERIC(5, 2);
+  nC NUMERIC(5, 2);
+  delta NUMERIC(10, 2); 
+  limite_inferior INTEGER := 1;
+  limite_superior INTEGER := 10;
+BEGIN
+
+  nA := random() * (limite_superior - limite_inferior) + limite_inferior;
+  nB := random() * (limite_superior - limite_inferior) + limite_inferior;
+  nC := random() * (limite_superior - limite_inferior) + limite_inferior;
+
+  delta := (nB ^2) - (4 * nA * nC);
+
+  RAISE NOTICE 'nA: %, nB: %, nC: %, delta: %', nA, nB, nC, delta;
+END $$;
+
+
+--1.3 Faça um programa que gere um valor real no intervalo [20, 30] que representa uma temperatura em graus Celsius. Faça a conversão para Fahrenheit e exiba.
+DO $$
+DECLARE
+  n1 NUMERIC(5, 2);
+  limite_inferior INTEGER := 20;
+  limite_superior INTEGER := 30;
+  fahrenheit NUMERIC(5, 2);
+BEGIN
+  n1 := random() * (limite_superior - limite_inferior) + limite_inferior;
+
+  fahrenheit := (n1 * 1.8) + 32;
+
+  RAISE NOTICE 'Celsius: %, Fahrenheit: %', n1, fahrenheit;
+END $$;
+
+
+--1.2 Faça um programa que gere um valor real e o exiba.
+DO $$
+DECLARE
+  n1 NUMERIC(5, 2);
+  limite_inferior NUMERIC := 1;
+  limite_superior NUMERIC := 10;
+BEGIN
+  n1 := random() * (limite_superior - limite_inferior) + limite_inferior;
+  RAISE NOTICE 'Valor gerado: %', n1;
+END $$;
+
+
+--1.1 Faça um programa que gere um valor inteiro e o exiba.
+ DO $$
+DECLARE
+  n2 INTEGER;
+  limite_inferior INTEGER := 1;
+  limite_superior INTEGER := 100;
+BEGIN
+  n2 := floor(random() * (limite_superior - limite_inferior + 1) + limite_inferior)::INTEGER;
+  RAISE NOTICE 'Valor de n2: %', n2;
+END $$;
+
+
+
+
+
+
+--DO $$
+--DECLARE
+   -- n1 NUMERIC (5, 2);
+  --  n2 INTEGER;
+ --   limite_inferior INTEGER := 5;
+ --   limite_superior INTEGER := 17;
+--BEGIN
+-- 0 <= n1 < 1 (real) entre 0 e 1 execto o 1 o zero incluso o 1 nao 
+--    n1 := random();
+   -- RAISE NOTICE 'n1 := radom apenas entre 0 e 1 execto o 1 o zero incluso o 1 nao : %', n1;
+-- 1 <= n1 < 10 (real) nao inclui o 10 inclui no minimo o 1
+  --  n1 := 1 + random() * 9 ;
+   -- RAISE NOTICE 'n1: 1+radom*9 nao inclui o 10 tem como minimo 1 :  %', n1;
+-- 1 <= n2 <10 (:: faz type cast é uma função que converte um valor de um tipo de dado para outro ) (floor arredonda para baixo) cortar a parte decimal
+   -- n2 := floor(random() * 10 + 1)::int;
+  --  RAISE NOTICE 'n2 cortar a parte decimal : %', n2;
+-- limite_inferior <= n2 <= limite_superior  superior menos o inferior mais um vai dar ate 12.999 mas com tem o chao fica 12 nao chega ate o 13 no caso 13 numros acima do 5 que é o 16.9999 ou 16 com o chao 
+  --  n2 := floor(random() * (17 - 5 + 1) + 5)::int;
+  --  RAISE NOTICE '%', n2;
+-- limite_inferior <= n2 <= limite_superior
+  --  n2 := floor(random() * (limite_superior - limite_inferior + 1) + limite_inferior)::int;
+  --  RAISE NOTICE '%', n2;
+--END $$;
 
 
 --DO 
--- ap_09_estruturas_de_selecao/script.sql
+++ ap_09_estruturas_de_selecao/script.sql
@@ -1,10 +1,138 @@
---CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
---INT) RETURNS INT AS
---$$
---BEGIN
---RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
---END;
---$$ LANGUAGE plpgsql;
+--dd/mm/aaaa é valida?
+DO $$
+DECLARE
+    --testar
+    --22/10/2022:valida
+    --29/02/2020: VALIDA
+    --29/02/2021: invalida
+    --28/02/2021: valia
+    --31/09/2025: invalida
+    --31/06/2025: invalida
+    data INT := 21122005;
+    dia INT;
+    mes INT;
+    ano INT;
+    data_valida BOOL := True;
+
+BEGIN
+    dia:= data / 1000000;
+    mes := data % 1000000/10000;
+    ano := data %10000;
+    RAISE NOTICE '% / % / %', dia, mes, ano;
+    IF ano >= 1 THEN
+        CASE 
+            WHEN mes > 12 OR mes < 1 Or dia < 1 OR dia > 31 THEN  data_valida := FALSE ;
+            ELSE 
+                IF (mes IN (4, 6, 9, 11 )) And dia > 30 THEN data_valida := FALSE; 
+                ELSE
+                    IF mes = 2 THEN 
+                    CASE 
+                        WHEN  ano % 4 = 0 AND ano % 100 <> 0 OR ano % 400 = 0 THEN 
+                            IF dia > 29 THEN data_valida := FALSE;
+                            END IF;
+                        ELSE 
+                            IF dia > 28 THEN data_valida := FALSE;
+                            END IF ;
+                    END CASE;
+                END IF ; 
+            END IF;
+        END CASE;
+    ELSE
+        data_valida := FALSE;
+    END IF;
+    CASE   
+        WHEN data_valida THEN RAISE NOTICE 'Data Valida';
+        ELSE RAISE NOTICE 'Data Invalida';
+    END CASE;
+END;
+$$
+
+
+DO $$
+DECLARE
+    valor INT :=valor_aleatorio_enrre (1,12);
+    mensagem VARCHAR(200);
+BEGIN
+    RAISE NOTICE ' o VALOR DE VEZ É %', valor;
+    CASE valor
+        WHEN 1, 3, 5, 7, 9, THEN
+            mensagem := 'impar';
+        WHEN 2,4,6,8,10
+            mensagem := 'par';
+        ELSE
+        mensagem := 'nao está no intervalo';
+    END CASE;
+    RAISE NOTICE '%', mensagem 
+END; 
+$$
+
+
+
+
+DO 
+$$
+DECLARE
+    a INT := valor_aleatorio_entre(0,20);
+    b INT := valor_aleatorio_entre(0,20);
+    c INT := valor_aleatorio_entre(0,20);
+    delta NUMERIC(10,2);
+    raizUm NUMERIC(10,2);
+    raizDois NUMERIC(10,2);
+BEGIN
+-- ax^2 + bx + c = 0
+    RAISE NOTICE '%x% + %x + % = 0', a, U&'\00b2', b, c;
+    IF a = 0 THEN 
+        RAISE NOTICE 'Não é equação do segundo grau';
+    ELSE 
+        delta := b ^ 2 - 4 * a * c;
+        RAISE NOTICE 'Delta: %', delta;
+        IF delta < 0 THEN 
+            RAISE NOTICE 'Não tem raiz';
+        ELSE IF delta = 0 THEN
+            raizUm := -1 * b / (2 * a);
+            RAISE NOTICE 'Teve uma  raiz. Essa aqui: %', raizUm;
+        ELSE 
+            raizUm := (-b + |/delta) / (2 * a);
+            raizUm := (-b - |/delta) / (2 * a);
+            RAISE NOTICE 'Duas raízes. Observe: % e %', raizUm, raizDois;
+        END IF; 
+    END IF; 
+-- U&'\00b2' = elevado ao quadrado
+-- a equivale ao primeiro %, 
+END;$$
+
+DO
+$$
+DECLARE
+    v_valor INT;
+BEGIN
+    v_valor := valor_aleatorio_entre(1,30);
+    IF v_valor <= 20 THEN 
+        RAISE NOTICE 'A metade de % é %', v_valor, v_valor/2::FLOAT;
+    END IF;
+END;
+$$
+
+
+
+
+
+CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+INT) RETURNS INT AS
+$$
+BEGIN
+RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+INT) RETURNS INT AS
+$$
+BEGIN
+RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+END;
+$$ LANGUAGE plpgsql;
 
 DO
 $$
