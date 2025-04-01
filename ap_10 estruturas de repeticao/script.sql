2343072015fb61a09b7afaa3442450282b2e95c4
Isabela Lugo <isabela.lugo@fatec.sp.gov.br>
2025-04-01 11:06:46 -0300

    learn(ap10): encerra apostila

--- ap_10 estruturas de repeticao/script.sql
+++ ap_10 estruturas de repeticao/script.sql
@@ -1,5 +1,62 @@
 
 
+-- DO
+-- $$--FOREACH com fatias (slice)
+-- DO $$
+-- DECLARE
+--     vetor INT[] := ARRAY[1, 2, 3];
+--     matriz INT[] := ARRAY[
+--         [1, 2, 3],
+--         [4, 5, 6],
+--         [7, 8, 9]
+--     ];
+--     var_aux INT;
+--     vet_aux INT[];
+-- BEGIN
+--     RAISE NOTICE 'SLICE %, vetor', 0;
+--     FOREACH var_aux IN ARRAY vetor LOOP
+--         RAISE NOTICE '%', var_aux;    
+--     END LOOP;
+
+--     RAISE NOTICE 'SLICE %, vetor', 1;
+--     FOREACH vet_aux SLICE 1 IN ARRAY vetor LOOP
+--         RAISE NOTICE '%', vet_aux;
+--     END LOOP;
+
+--     RAISE NOTICE 'SLICE %, matriz', 0;
+--     FOREACH var_aux IN ARRAY matriz LOOP
+--         RAISE NOTICE '%', var_aux;    
+--     END LOOP;
+
+--     RAISE NOTICE 'SLICE %, matriz', 1;
+--     FOREACH vet_aux SLICE 1 IN ARRAY matriz LOOP
+--         RAISE NOTICE '%', vet_aux;
+--     END LOOP;
+
+--     RAISE NOTICE 'SLICE %, matriz', 2;
+--     FOREACH vet_aux SLICE 2 IN ARRAY matriz LOOP
+--         RAISE NOTICE '%', vet_aux;
+--     END LOOP;
+-- DECLARE
+-- i INT;
+-- j INT;
+-- BEGIN
+-- i := 0;
+-- <<externo>>
+-- LOOP
+-- i := i + 1;
+-- EXIT WHEN i > 10;
+-- j := 1;
+-- <<interno>>
+-- LOOP
+-- RAISE NOTICE '% %', i, j;
+-- j := j + 1;
+-- -- j vai contar até 5, o loop externo vai ser interrompido e oprograma acaba
+-- EXIT externo WHEN j > 5;
+-- END LOOP;
+-- END LOOP;
+-- END;
+-- $$
 
 
 
